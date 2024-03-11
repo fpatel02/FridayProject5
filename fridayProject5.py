6bbbed4a8f73ac9bb4f4482bfe532dbe3d8d13c2
fpatel02 <patelfauram1@outlook.com>
2024-03-10 23:16:53 -0500

    fetching database table name

---
+++ fridayProject5.py
@@ -0,0 +1,14 @@
+import sqlite3 as lite
+
+con = lite.connect('FridayProj5.py')
+
+with con:
+    cur = con.cursor()
+    cur.execute('SELECT SQLITE_VERSION()')
+    data = cur.fetchone()
+    print(f"SQLite version: {data[0]}")
+
+    print("Table records:")
+    cursor = con.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+    print(cursor.fetchall())
